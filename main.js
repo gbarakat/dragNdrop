44d2b4fcd1e3d1bf6f96ecc53f63733691519384
gbarakat <gehadbarakaat@gmail.com>
2019-05-29 17:27:40 +0200

    second commit

--- main.js
+++ main.js
@@ -1,29 +1,63 @@
 function doFirst(){
-    boxImage = document.getElementByclassName('boxImage');
-    
-    for(var i =0; i< BoxIamge.lengt; i++){
-        BoxIamge[i].addEventListener("dragstart", startDrag , false);
+    boxImage = document.getElementsByClassName("boxImage")
+    for(var i =0; i< boxImage.length; i++){
+        boxImage[i].addEventListener("dragstart", startDrag , false);    
     }
-    leftbox = document.getElementsByClassName('leftbox');
-   
-    for(var i =0; i< leftbox.lengt; i++){
-    leftbox[i].addEventListener("dragenter", function(e){e.preventDefault()}, false);
-    leftbox[i].addEventListener("dragover", function(e){e.preventDefault()}, false);
-    leftbox[i].addEventListener('drop', dropped , false);
+    leftbox = document.getElementsByClassName('leftbox'); 
+
+    for(var i = 0; i< leftbox.length; i++){
+            
+            leftbox[i].addEventListener("dragenter",function(e){
+                    e.preventDefault(); 
+                      e.target.style.backgroundColor="white";
+                    },
+                false
+            );
+    
+            leftbox[i].addEventListener("dragover", function(e){
+                    e.preventDefault();
+                        e.target.style.backgroundColor="gray";
+                    },
+                false
+            );
+            
+            leftbox[i].addEventListener("dragleave", function(e){
+                e.preventDefault();
+                    e.target.style.backgroundColor="white";
+                    },
+                false
+                );
+            
+            leftbox[i].addEventListener('drop',dropped, false);
    }
 
 function startDrag(e){
-    var code = ['<img src="http://source.unsplash.com/random/151x151">',
-                '<img src="http://source.unsplash.com/random/149x149">',
-                '<img src="http://source.unsplash.com/random/150x150">']
-    if (e.target == boxImage[i]){
-            e.dataTransfer.setData('Text', code);
-        }
+        var code = ["html.png/",
+                    "js.png",
+                    "css.png"]    ]
+
+        //for(var i=0; i< code.length; i++){
+            
+           /* if (e.target.value == boxImage[i]){
+                console.log(code[i]);
+                e.dataTransfer.setData('Text', code[i]);
+                
+        }*/
+       
+        
+    //}
+}
 
 function dropped(e){
     e.preventDefault();
-    leftbox.innerHTML = e.dataTransfer.getData('Text');
-}
-
+    for(var i =0; i<leftbox.length; i++){
+        if(e.target == leftbox[i]){
+            console.log(boxImage[i])
+            boxImage[i].style.filter= "grayscale(100%)";
+            leftbox[i].innerHTML =  '<img src=' + e.dataTransfer.getData('Text') +'/>';
+            }
+            }
+        
+            }
+    }
 window.addEventListener("load", doFirst , false);
-
