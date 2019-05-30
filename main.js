fd8023d1364cc32a980b5a6811fcb96ae9a330fa
gbarakat <gehadbarakaat@gmail.com>
2019-05-30 23:31:44 +0200

    drag and drop bug fixed

--- main.js
+++ main.js
@@ -1,9 +1,62 @@
+(function timerHandler(){
+    const SECONDHAND = document.getElementById("second");
+    const Clockbg = document.getElementById("circle")
+    const stpIntrvalBtn = document.getElementById("stpInterval")
+    
+    let sec = 0;
+    let secPosition = sec*360/60;
+    let stopInterval = false;
+    let click= 0;
+
+    function runTheClock() {
+        console.log(click);
+        if(!stopInterval || click == 0)
+        {
+            secPosition = secPosition+6;
+            click=0;
+            if (secPosition > 210){
+
+                    Clockbg.style.fill = "red";
+            }
+            if (secPosition == 360){
+                secPosition=0;
+                Clockbg.style.fill = "green";
+                //score -=100
+            if(stpIntrvalBtn.innerHTML="Play Timer"){
+                stpIntrvalBtn.innerHTML="Stop Timer"
+            }    
+        }
+        SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+        } 
+
+    }
+
+    setInterval(runTheClock, 1000);
+
+    stpIntrvalBtn.addEventListener("click", stopTimer)
+
+    function stopTimer(){
+            stopInterval = true;
+            if(click==0 ){
+                //when the timer is stopped
+                click++;
+                stpIntrvalBtn.innerHTML="Play Timer";
+            }else{
+                 //when the timer is running
+                click=0;
+                stpIntrvalBtn.innerHTML="Stop Timer";
+            }
+
+    } 
+})();
+
 function doFirst(){
-    boxImage = document.getElementsByClassName("boxImage")
+    var currentObject ;
+    var boxImage = document.getElementsByClassName("boxImage")
     for(var i =0; i< boxImage.length; i++){
         boxImage[i].addEventListener("dragstart", startDrag , false);    
     }
-    leftbox = document.getElementsByClassName('leftbox'); 
+    var leftbox = document.getElementsByClassName('leftbox'); 
 
     for(var i = 0; i< leftbox.length; i++){
             
@@ -31,33 +84,17 @@ function doFirst(){
             leftbox[i].addEventListener('drop',dropped, false);
    }
 
-function startDrag(e){
-        var code = ["html.png/",
-                    "js.png",
-                    "css.png"]    ]
+    function startDrag(e){
+        currentObject = e.target;
+        }
 
-        //for(var i=0; i< code.length; i++){
-            
-           /* if (e.target.value == boxImage[i]){
-                console.log(code[i]);
-                e.dataTransfer.setData('Text', code[i]);
-                
-        }*/
-       
-        
-    //}
-}
-
-function dropped(e){
-    e.preventDefault();
-    for(var i =0; i<leftbox.length; i++){
-        if(e.target == leftbox[i]){
-            console.log(boxImage[i])
-            boxImage[i].style.filter= "grayscale(100%)";
-            leftbox[i].innerHTML =  '<img src=' + e.dataTransfer.getData('Text') +'/>';
-            }
-            }
-        
-            }
+    function dropped(evt){
+        evt.preventDefault();
+        for(var i =0; i<leftbox.length; i++){
+                evt.target.innerHTML = " ";
+                evt.target.append(currentObject);
+                currentObject.removeEventListener("dragstart", startDrag, false);    
+                    }
+                }
     }
 window.addEventListener("load", doFirst , false);
